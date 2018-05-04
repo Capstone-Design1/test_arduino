4a6a1356bd19934396031ee3353e8e6e5dfb814b
stonehye <stonehye@gmail.com>
2018-05-04 11:33:44 +0900

    Add files via upload

---
+++ PIRsensor.ino
@@ -0,0 +1,23 @@
+int pirPin = D8;
+int val;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(D2, OUTPUT);
+}
+
+void loop() {
+val = digitalRead(pirPin); //read state of the PIR
+
+if (val == LOW) {
+digitalWrite(D2, LOW);
+Serial.println("LOW");
+}
+else {
+digitalWrite(D2, HIGH);
+Serial.println("HIGH");
+}
+delay(1000); 
+
+} 
+
